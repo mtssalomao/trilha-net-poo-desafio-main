c1d7e5080a44c8ae3ace841893f9112628b71332
Matheus Salomão <98296114+mtssalomao@users.noreply.github.com>
2024-07-12 14:07:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using DesafioPOO.Models;
+
+
+Iphone iphone = new Iphone("","","",0); 
+Nokia nokia = new Nokia("","","",0);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("nomeApp");
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("nomeApp");
+
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
